2773d9564d582e4fd3feb66ae1cfe75b8f8e6030
kubotak0630 <kubotak@f01.itscom.net>
2019-06-15 22:12:21 +0900

    3rd commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+const outputPath = path.resolve(__dirname, 'dist')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: outputPath
+  }
+}
