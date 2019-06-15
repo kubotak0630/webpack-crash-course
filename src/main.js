a9cdbc949a37076047bf0342ff09d5053084ebea
kubotak0630 <kubotak@f01.itscom.net>
2019-06-15 23:10:44 +0900

    Implement webpack-dev-server

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import _ from 'lodash'
+import { Nijou } from './utilities'
+
+console.log(Nijou(2));
 
 function component() {
   const element = document.createElement('div');
-  const array = ['Hello', 'webpack'];
+  const array = ['Hello', 'webpack', '!!'];
   element.innerHTML = _.join(array, ' ');
   return element;
 }
