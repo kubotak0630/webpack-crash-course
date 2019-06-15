cc9018f4058400a279d3fe89d1a33fb2aa8e7dfc
kubotak0630 <kubotak@f01.itscom.net>
2019-06-15 23:31:40 +0900

    Implement modules 2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import _ from 'lodash'
-import { Nijou } from './utilities'
+// import { Nijou, NAME } from './utilities'
+import * as utls from './utilities'
+import Lion1 from './utilities'
 
-console.log(Nijou(2));
+console.log(utls.Nijou(2));
+console.log(utls.NAME);
+console.log(Lion1.say());
 
 function component() {
   const element = document.createElement('div');
